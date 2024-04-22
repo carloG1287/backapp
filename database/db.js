64995f0bb955769bd56010b8cf13c5d80cc14a1a
carloG1287 <carlox1287@gmail.com>
2024-04-21 22:07:36 -0400

    Agregar configuración de variables de entorno en db.js

--- database/db.js
+++ database/db.js
@@ -1,5 +1,7 @@
 import {Sequelize} from "sequelize";
+import 'dotenv/config'
 
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST)
 const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql'
