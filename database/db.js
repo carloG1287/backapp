ef300cf0f971279e755f9f567058e0c2496dbf06
carloG1287 <carlox1287@gmail.com>
2024-04-21 22:17:51 -0400

    Agregar configuración de puerto en db.js

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 import {Sequelize} from "sequelize";
 import 'dotenv/config'
 
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST)
 const db = new Sequelize( process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: Number.parseInt(process.env.DB_PORT)
 });
 
 export default db
